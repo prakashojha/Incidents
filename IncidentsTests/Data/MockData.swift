b7cf868f242498bc6f3d86bb06a1830c19d5cd21
Bindu <prakashojha2005@gmail.com>
2022-10-18 12:30:47 +1300

    modified network Mock Test

--- IncidentsTests/Data/MockData.swift
+++ IncidentsTests/Data/MockData.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct MockData{
+    
+    static let data1 = """
+            {
+                "title": "House Fire - WATSON",
+                "callTime": "2022-07-06T12:35:12+1000",
+                "lastUpdated": "2022-07-06T12:44:10+1000",
+                "id": "010017-06072022",
+                "latitude": -35.24175205,
+                "longitude": 149.15683313,
+                "description": "Structure fire - single story\n3 Crews On Scene",
+                "location": "DOWLING STREET, WATSON, 2602",
+                "status": "On Scene",
+                "type": "House Fire",
+                "typeIcon": "https://i.imgur.com/IUwhSWJ.png"
+            }
+    """
+    
+    static var response: String{
+        return data1
+    }
+}
--- IncidentsTests/Data/MockUrlProtocol.swift
+++ IncidentsTests/Data/MockUrlProtocol.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+class MockURLProtocol: URLProtocol{
+    
+    static var stubResponse: Data?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        self.client?.urlProtocol(self, didLoad: MockURLProtocol.stubResponse ?? Data())
+        self.client?.urlProtocolDidFinishLoading(self)
+    }
+    
+    override func stopLoading() {
+    }
+}
